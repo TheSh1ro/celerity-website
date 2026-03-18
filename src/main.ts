936f93bfa2223f19ce571f6a8949a6074f6b9c05
thesh1ro <gabrielmonteiro1111@gmail.com>
2026-03-18 09:08:54 -0300

    feat: adiciona error handler global para sessão inválida

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,18 @@ import '@/assets/global.css'
 
 import App from './App.vue'
 import router from './router'
+import { SessionInvalidError } from '@/errors'
+import { useAuthStore } from '@/stores/auth'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.config.errorHandler = (err) => {
+  if (err instanceof SessionInvalidError) {
+    useAuthStore().logout()
+  }
+}
 
 app.mount('#app')
